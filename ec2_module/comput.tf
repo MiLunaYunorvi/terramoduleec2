3fb95029782e4bddadb1052648d279dac1575d41
MiLunaYunorvi <misusuyatec@gmail.com>
2023-09-30 19:22:52 -0500

    new name 2

--- ec2_module/comput.tf
+++ ec2_module/comput.tf
@@ -10,9 +10,9 @@ resource "aws_instance" "module1" {
 
     ami = var.mod_instance_parameters["instance_ami"]
     instance_type = var.mod_instance_parameters["instance_type"]
-    # tags = {
-    #     Name = var.mod_instance_parameters["name"]
-    # }
+    tags = {
+        Name = var.mod_instance_parameters["name"]
+    }
     key_name = var.mod_instance_parameters["key_name"]   
 }
 
